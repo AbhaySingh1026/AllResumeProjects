4d0dedeac1b6704d522255a30485bff63e674ab0
nxtElonMusk <gourav7398@gmail.com>
2022-12-03 12:11:58 +0530

    removed final assess.'

--- FINAL ASSESMENT/AIRLINE .WEBAPP/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AIRLINE_.WEBAPP.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
